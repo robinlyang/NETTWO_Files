706f3387dd33d9db38434acdf7845235d9018d28
robinlyang <robiny@gmail.com>
2016-11-21 19:28:12 -0700

    More saving

--- NETTWO_Files/Program.cs
+++ NETTWO_Files/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace NETTWO_Files
 {
@@ -10,6 +11,20 @@ namespace NETTWO_Files
     {
         static void Main(string[] args)
         {
+            //File class
+            //Directory Class
+
+            string[] dir = Directory.GetDirectories(@"C:\");
+            foreach(string name in dir)
+            {
+                Console.WriteLine(name);
+            }
+            string[] files = Directory.GetFiles(@"C:\Users\ryang\Downloads");
+            foreach (string name in files)
+            {
+                Console.WriteLine(name);
+            }
+            Console.Read();
         }
     }
 }
